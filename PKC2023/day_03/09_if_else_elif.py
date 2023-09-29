62ddd16931ddf53464d2b58753c783d2006d95e6
LAPTOP INSIDE <asifbwp917@gmail.com>
2023-09-29 23:04:22 +0500

    Correct

---
+++ PKC2023/day_03/09_if_else_elif.py
@@ -0,0 +1,14 @@
+required_age_at_school = 5
+anas_age = 3
+
+# question can anas go to school
+
+if anas_age == required_age_at_school:
+    print("Congratulation! anas can join the school")
+elif anas_age > required_age_at_school:
+        print("anas cannot go to school or should join the higher classes")
+elif anas_age <= 2:
+    print("you should take care of anas, he is still a baby")
+else:
+    print("anas cannot go to school")
+ 
